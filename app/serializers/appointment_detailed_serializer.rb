cb910e90dc13cd6026893b98087e15bdb80958d1
Wesley-waka <wesleywaka77@gmail.com>
2023-12-28 07:40:33 +0300

    Serializer:appointment_detailed

---
+++ app/serializers/appointment_detailed_serializer.rb
@@ -0,0 +1,6 @@
+class AppointmentDetailedSerializer < ActiveModel::Serializer
+  attributes :id, :date, :approved, :appointment_type, :appointment_info, :time
+  belongs_to :user
+  belongs_to :practitioner
+  has_many :messages
+end
