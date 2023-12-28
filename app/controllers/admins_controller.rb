5c70073362d283a59d588ca3ad74e773ec5fbce9
Wesley-waka <wesleywaka77@gmail.com>
2023-12-28 07:12:00 +0300

    Controller:admins

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,56 @@
+class AdminsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    skip_before_action :authorized, only: %i[create show]
+
+    def create
+        admin = Admin.create!(admin_params)
+        token = issue_token(admin, "admin")
+
+        admin_info = JSON.parse(
+            admin.to_json only: [:id, :username, :email]
+        )
+
+        render json: {admin: admin_info, jwt: token }, status: :created
+    end
+
+    def show
+        admin = Admin.find(params[:id])
+        token = issue_token(admin, "admin")
+
+        admin_info = JSON.parse(
+            admin.to_json only: [:id, :username, :email]
+        )
+
+        render json: {admin: admin_info, jwt: token }, status: :created
+    end
+
+    def update
+        admin = Admin.find(params[:id])
+        admin.update(admin_params)
+        render json: admin, status: :ok
+    end
+
+    def destroy
+        admin = Admin.find(params[:id])
+        admin.destroy
+        head :no_content
+    end
+
+    private
+    def authorize
+        User.find(session[:user_id])
+    end
+
+    def admin_params
+        params.permit(:username, :email, :password)
+    end
+
+    def record_invalid(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    def record_not_found(not_found)
+        render json: not_found, status: 404
+    end   
+end
