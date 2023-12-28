5189efd44129c64703fac07ef78f578e7b06261b
Wesley-waka <wesleywaka77@gmail.com>
2023-12-28 07:24:06 +0300

    controller:practitioner_sessions

---
+++ app/controllers/practitioner_sessions_controller.rb
@@ -0,0 +1,31 @@
+class PractitionerSessionsController < ApplicationController
+    skip_before_action :authorized, only: %i[create]
+    
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    def create
+        practitioner = Practitioner.find_by!(username: params[:username])
+        practitioner = practitioner.authenticate(params[:password])
+        token = issue_token(practitioner, "practitioner")
+        
+        practitioner_info = JSON.parse(
+            practitioner.to_json only: [:id, :username, :email],
+            include: [:appointments, :practitioner_profiles, :department]
+        )
+
+        render json: {practitioner: practitioner_info, jwt: token }, status: :created
+    end
+
+    def destroy
+        head :no_content
+    end
+
+    private
+    def user_params
+        params.permit(:username, :password)
+    end
+
+    def record_not_found(not_found)
+        render json: not_found, status: 404
+    end      
+end
