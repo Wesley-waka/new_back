06c2b42565227e894f79ed10c6e6d2d73e2e8e46
Wesley-waka <wesleywaka77@gmail.com>
2023-12-28 07:36:03 +0300

    Model:product

---
+++ app/models/product.rb
@@ -0,0 +1,31 @@
+class Product < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search_by_name,
+                  against: %i[name category],
+                  using: {
+                    tsearch: {
+                      prefix: true
+                    }
+                  }
+
+  def average_rating
+    reviews.average(:rating)
+  end
+
+  has_many :shopping_carts
+  has_many :orders, through: :shopping_carts
+  has_many :dosage_considerations
+  has_many :side_effects
+  has_many :reviews
+
+  has_many :shopping_carts, dependent: :destroy
+  has_many :orders, through: :shopping_carts, dependent: :destroy
+  has_many :dosage_considerations, dependent: :destroy
+  has_many :side_effects, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :price, presence: true
+end
